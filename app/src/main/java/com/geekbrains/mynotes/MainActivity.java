a275a2b20870517acb54bf87a8cd0975e0920097
Melian <79233450803@yandex.ru>
2022-04-13 18:36:22 +0700

    Создала каркас для приложения заметки

--- app/src/main/java/com/geekbrains/mynotes/MainActivity.java
+++ app/src/main/java/com/geekbrains/mynotes/MainActivity.java
@@ -2,6 +2,7 @@ package com.geekbrains.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null){
+            FragmentListNote fragmentListNote = new FragmentListNote();
+            getSupportFragmentManager().beginTransaction().replace(R.id.listNote, fragmentListNote).commit();
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                FragmentDescriptionNotes fragmentDescriptionNotes = new FragmentDescriptionNotes();
+                getSupportFragmentManager().beginTransaction().replace(R.id.descriptionNotes, fragmentDescriptionNotes).commit();
+            }
+        }
     }
 }
