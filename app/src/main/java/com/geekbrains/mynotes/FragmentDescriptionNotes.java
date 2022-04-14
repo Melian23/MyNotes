8c7336a52fae3fa480e27b8668bb196badadfdd7
Melian <79233450803@yandex.ru>
2022-04-14 16:41:30 +0700

    Встроила фрагменты в активити.

--- app/src/main/java/com/geekbrains/mynotes/FragmentDescriptionNotes.java
+++ app/src/main/java/com/geekbrains/mynotes/FragmentDescriptionNotes.java
@@ -4,12 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentDescriptionNotes extends Fragment {
+
+    public static final String ARG_NOTES = "notes";
+    private Notes notes;
+
+    public static FragmentDescriptionNotes newInstance(Notes notes) {
+        FragmentDescriptionNotes fragment = new FragmentDescriptionNotes();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(ARG_NOTES, notes);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,5 +34,27 @@ public class FragmentDescriptionNotes extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        String[] description = getResources().getStringArray(R.array.description);
+
+        for (int i = 0; i < description.length; i++) {
+            String descriptionNote = description[i];
+            TextView textView = new TextView(getContext());
+            textView.setTextSize(20f);
+            textView.setText(descriptionNote);
+            ((LinearLayout) view).addView(textView);
+        }
+ /*       notes = getArguments().getParcelable(ARG_NOTES);
+
+        TextView etName = view.findViewById(R.id.textName);
+        etName.setText(notes.getName());
+
+        TextView etData =  view.findViewById(R.id.textData);
+        etData.setText(notes.getData());
+
+        EditText etDescription = view.findViewById(R.id.textDescription);
+        etDescription.setText(notes.getDescription());
+*/
     }
 }
--- app/src/main/java/com/geekbrains/mynotes/MainActivity.java
+++ app/src/main/java/com/geekbrains/mynotes/MainActivity.java
@@ -13,10 +13,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null){
-            FragmentListNote fragmentListNote = new FragmentListNote();
-            getSupportFragmentManager().beginTransaction().replace(R.id.listNote, fragmentListNote).commit();
+            FragmentListNotes fragmentListNotes = FragmentListNotes.newInstance();
+            getSupportFragmentManager().beginTransaction().replace(R.id.listNote, fragmentListNotes).commit();
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-                FragmentDescriptionNotes fragmentDescriptionNotes = new FragmentDescriptionNotes();
+                Notes defaultNote = new Notes("Тост", 2022, "Удивительно устроен человек - он огорчается, когда теряет богатство, и равнодушен к тому, что безвозвратно уходят дни его жизни!");
+                FragmentDescriptionNotes fragmentDescriptionNotes = FragmentDescriptionNotes.newInstance(defaultNote);
                 getSupportFragmentManager().beginTransaction().replace(R.id.descriptionNotes, fragmentDescriptionNotes).commit();
             }
         }
